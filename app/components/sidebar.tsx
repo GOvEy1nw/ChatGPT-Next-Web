d51cce8732d88992ca27dfda4410b9c03b8ef006
GOvEy1nw <45126527+GOvEy1nw@users.noreply.github.com>
2023-10-22 16:38:51 +0100

    logo update

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -145,7 +145,7 @@ export function SideBar(props: { className?: string }) {
       }`}
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
-        <div className={styles["sidebar-title"]} data-tauri-drag-region>
+        <div className={styles["sidebar-title"]  + " no-dark"} data-tauri-drag-region>
         <RaisLogo />
         </div>
       </div>
