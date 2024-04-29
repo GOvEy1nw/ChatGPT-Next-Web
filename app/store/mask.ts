d85a38f80fb03b22f705b26176634405837650fe
GOvEy1nw <45126527+GOvEy1nw@users.noreply.github.com>
2024-04-29 14:27:00 +0100

    Update mask.ts

--- app/store/mask.ts
+++ app/store/mask.ts
@@ -11,7 +11,7 @@ export type Mask = {
   createdAt: number;
   avatar: string;
   name: string;
-  description?: string;
+  description: string;
   hideContext?: boolean;
   context: ChatMessage[];
   syncGlobalConfig?: boolean;
